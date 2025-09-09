27f56676bbd249836f4c6421901dfdce147ea6f0
Thongnarin Yeamsri(Ball) <minithong68@gmail.com>
2025-09-09 14:59:23 +0700

    merge promotion

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div style={{ background: '#141414', flex: 1 , minHeight: '100vh'}}>
       <Navbar />
       <div style={{ padding: '16px' }}>
-        <div style={{ background: 'linear-gradient(90deg, #9254de 0%, #f759ab 100%)', height: 180, borderRadius: 10, marginBottom: 24 }}></div>
+        <div style={{ background: 'linear-gradient(90deg, #9254de 0%, #f759ab 100%)', height: 180, borderRadius: 10, marginBottom: 24, flex: 1}}></div>
         <Title level={3} style={{ color: 'white' }}>Product</Title>
         <Space style={{ marginBottom: 16 }}>
           <Button type="primary" shape="round">Top</Button>
