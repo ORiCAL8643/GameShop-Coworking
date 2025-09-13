c920edbe75fb6f2e9af6b957fd1397d228c098df
Punyaphat Kethom(Poon) <poonaugust1007@gmail.com>
2025-09-13 17:32:23 +0700

    Edit workshop system almost complete

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,7 +11,7 @@ import PromotionBanner from "../components/PromotionBanner";
 const { Title } = Typography;
 
 const Home = () => {
-  const { id:userId } = useAuth();
+  const { userId } = useAuth();
   const [promotions, setPromotions] = useState<Promotion[]>([]);
   const navigate = useNavigate();
 
